104b60918cff0e6caf16a992340a7b3d6af6500d
tgiguashvili <tgiguashvili@users.noreply.github.com>
2023-05-12 19:44:58 +0400

    full

---
+++ src/script.js
@@ -0,0 +1,14 @@
+var salary=prompt("ამწუთას თქვენი ანაზღაურება შეადგენს?")
+var really=prompt("კაი ნუმატყუებ))")
+var sum=parseInt(salary)
+var really=parseInt(really)
+if(really<505){
+    alert("ეუფ..:| კაი მიდი შეავსე ინფო და რაღაცა იქნება))")
+
+}
+else if(really>505 && really<905){
+    alert("ნუ წუწუნებ მთლად ცუდათაც არყოფილა საქმე :) შეავსე ფორმა და იქნებ უკეთესი გინახოთ რამე ")
+}
+else if(really>905){
+    alert("რაგენაღვლება )) ")
+}
